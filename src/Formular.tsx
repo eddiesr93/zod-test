65347c97e048efd9a6b68f3e286a2314e8c373a6
Eddie Roman <eddie.roman@stsnet.ro>
2024-10-31 09:42:38 +0200

    more validation

--- src/Formular.tsx
+++ src/Formular.tsx
@@ -4,25 +4,39 @@ import { z } from './validation';
 
 export default function Formular() {
   const baseSchema = z.object({
+    name: z.string().optional(),
     lastName: z.string().optional(),
     role: z.union([z.literal('admin'), z.literal('user')]),
-    phone: z.string().min(1).phone(),
+    phone: z.string().optional(),
     alphaNumeric: z.string().alphaNumeric(),
-    cnp: z.string().cnp(),
+    cnp: z.string().optional(),
   });
 
-  const validationSchema = baseSchema.refine(
-    (data) => {
-      if (data.role === 'admin') {
-        return data.lastName && data.lastName.length >= 6;
+  const validationSchema = baseSchema.superRefine((form, ctx) => {
+    if (form.role === 'user') {
+      if (!form.name) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "User's name is required",
+          path: ['name'],
+        });
       }
-      return true;
-    },
-    {
-      message: "Admin's last name must be at least 6 characters long",
-      path: ['lastName'],
+      if (!form.lastName) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "User's last name is required",
+          path: ['lastName'],
+        });
+      }
+      if (!form.cnp || !z.string().cnp().safeParse(form.cnp).success) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "User's CNP is required and must be valid",
+          path: ['cnp'],
+        });
+      }
     }
-  );
+  });
 
   const {
     handleSubmit,
@@ -55,6 +69,12 @@ export default function Formular() {
         {...register('lastName')}
       />
       <p className={'text-red-500'}>{errors?.lastName?.message}</p>
+      <label>Prenume</label>
+      <input
+        className={'border border-blue-400 rounded-md py-1'}
+        {...register('name')}
+      />
+      <p className={'text-red-500'}>{errors?.name?.message}</p>
       <label>Telefon</label>
       <input
         className={'border border-blue-400 rounded-md py-1'}
